120bef8ee619c07b927671c30c553491a59ae093
Bilal Ifzal <your-email@example.com>
2026-03-29 18:26:38 +0500

    Add all ecommerce project files including images and html

---
+++ index.js
@@ -0,0 +1,7 @@
+// var bar=document.getElementById('bar');
+// var nav=document.getElementsByClassName("nav-list")
+// if(bar){
+//     bar.addEventListener('click' ,() =>{
+//         nav.classList.add('active')
+//     })
+// }
